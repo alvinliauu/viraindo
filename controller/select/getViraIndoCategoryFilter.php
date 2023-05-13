af8ad3c170122856434e8a6eeb893644e218878a
alvin <alvinliauu@gmail.com>
2023-05-13 22:44:47 +0700

    kategori filter

--- controller/select/getViraIndoCategoryFilter.php
+++ controller/select/getViraIndoCategoryFilter.php
@@ -27,7 +27,6 @@
             }
 
             $arr = explode(" ", $this->name);
-            $thecategory = $this->category;
 
             if($jsonInput == null){
                 echo "item not found";
@@ -37,8 +36,8 @@
                     $arrTotal = "";
                     foreach($arr as $index => $count){
                         if($index == 0){
-                            $arrTotal .= "SELECT TVC.category_name, TVSC.sub_category_id, TVSC.sub_category_name, GROUP_CONCAT(TVI.item_id ORDER BY TVI.item_id ASC SEPARATOR '$^$') AS item_id, GROUP_CONCAT(TVI.item_name ORDER BY TVI.item_id ASC SEPARATOR '$^$') AS item_name,
-                            GROUP_CONCAT(TVI.item_picture ORDER BY TVI.item_id ASC SEPARATOR '$^$') AS item_picture, GROUP_CONCAT(TVI.item_new_price ORDER BY TVI.item_id ASC SEPARATOR '$^$') AS item_price
+                            $arrTotal .= "SELECT TVC.category_name, TVSC.sub_category_id, TVSC.sub_category_name, GROUP_CONCAT(TVI.item_id ORDER BY TVI.item_new_price $this->price SEPARATOR '$^$') AS item_id, GROUP_CONCAT(TVI.item_name ORDER BY TVI.item_new_price $this->price SEPARATOR '$^$') AS item_name,
+                            GROUP_CONCAT(TVI.item_picture ORDER BY TVI.item_new_price $this->price SEPARATOR '$^$') AS item_picture, GROUP_CONCAT(TVI.item_new_price ORDER BY TVI.item_new_price $this->price SEPARATOR '$^$') AS item_price
                             FROM tbl_viraindo_item TVI JOIN tbl_viraindo_sub_category TVSC ON TVI.sub_category_id = TVSC.sub_category_id
                             JOIN tbl_viraindo_category TVC ON TVC.category_id = TVSC.category_id
                             WHERE TVI.item_name LIKE '%$count%'
@@ -50,18 +49,18 @@
                         $arrTotal .= $arrLoop;
                     }           
     
-                    $sqlQuery = "$arrTotal AND TVSC.sub_category_id = $this->id GROUP BY TVSC.sub_category_id ORDER BY TVI.item_new_price $this->price;";
+                    $sqlQuery = "$arrTotal AND TVSC.sub_category_id = $this->id GROUP BY TVSC.sub_category_id;";
     
                     $stmt = $this->conn->prepare($sqlQuery);
                     $stmt->execute();
                     return $stmt;
                 }
                 else{
-                    $sqlQuery = "SELECT TVC.category_name, TVSC.sub_category_id, TVSC.sub_category_name, GROUP_CONCAT(TVI.item_id ORDER BY TVI.item_id ASC SEPARATOR '$^$') AS item_id, GROUP_CONCAT(TVI.item_name ORDER BY TVI.item_id ASC SEPARATOR '$^$') AS item_name,
-                    GROUP_CONCAT(TVI.item_picture ORDER BY TVI.item_id ASC SEPARATOR '$^$') AS item_picture, GROUP_CONCAT(TVI.item_new_price ORDER BY TVI.item_id ASC SEPARATOR '$^$') AS item_price
+                    $sqlQuery = "SELECT TVC.category_name, TVSC.sub_category_id, TVSC.sub_category_name, GROUP_CONCAT(TVI.item_id ORDER BY TVI.item_new_price $this->price SEPARATOR '$^$') AS item_id, GROUP_CONCAT(TVI.item_name ORDER BY TVI.item_new_price $this->price SEPARATOR '$^$') AS item_name,
+                    GROUP_CONCAT(TVI.item_picture ORDER BY TVI.item_new_price $this->price SEPARATOR '$^$') AS item_picture, GROUP_CONCAT(TVI.item_new_price ORDER BY TVI.item_new_price $this->price SEPARATOR '$^$') AS item_price
                     FROM tbl_viraindo_item TVI JOIN tbl_viraindo_sub_category TVSC ON TVI.sub_category_id = TVSC.sub_category_id
                     JOIN tbl_viraindo_category TVC ON TVC.category_id = TVSC.category_id
-                    WHERE TVSC.sub_category_id = $this->id GROUP BY TVSC.sub_category_id ORDER BY TVI.item_new_price $this->price;";
+                    WHERE TVSC.sub_category_id = $this->id GROUP BY TVSC.sub_category_id;";
                     $stmt = $this->conn->prepare($sqlQuery);
                     
                     $stmt->execute();
