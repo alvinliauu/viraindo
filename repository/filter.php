f90d705132a7ba01f6478c53c0a68cc4d9c63066
alvin <alvinliauu@gmail.com>
2023-05-15 22:21:28 +0700

    test

--- repository/filter.php
+++ repository/filter.php
@@ -34,6 +34,7 @@ function filter($category){
         $e = ["1STPLAYER", "A4Tech", "AOC", "Aula", "Cooler Master", "Corsair", "Ducky", "Digital Alliance", "Logitech", "Powerlogic", "Razer", "Rexus", "SteelSeries", "HyperX"];
     }
 
+    print_r($category);
     $intersect = array_intersect($e, $arr);
     print_r($intersect);
     die();
