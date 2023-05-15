194401b14b8107a510d4e89b38ce22580d2aa58a
alvin <alvinliauu@gmail.com>
2023-05-15 23:02:49 +0700

    test

--- controller/select/getViraIndoCategoryFilter.php
+++ controller/select/getViraIndoCategoryFilter.php
@@ -64,9 +64,11 @@
                     $stmt = $this->conn->prepare($sqlQuery);
                     $getStmt = $stmt->execute();
 
+                    print_r("test");
+                    die();
+
                     if(empty($getStmt)){
-                        print_r("test");
-                        die();
+                        
                         $arrTotal .= "SELECT TVC.category_name, TVSC.sub_category_id, TVSC.sub_category_name
                         FROM tbl_viraindo_item TVI JOIN tbl_viraindo_sub_category TVSC ON TVI.sub_category_id = TVSC.sub_category_id
                         JOIN tbl_viraindo_category TVC ON TVC.category_id = TVSC.category_id WHERE TVSC.sub_category_id = $this->id;";
