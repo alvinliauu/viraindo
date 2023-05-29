0f4d17eefaf3a953d12abed9f6dd3ac295178610
alvin <alvinliauu@gmail.com>
2023-05-29 21:04:18 +0700

    test

--- main/excel/index.php
+++ main/excel/index.php
@@ -1,6 +1,7 @@
 <?php
-print_r("test");die();
+
 require_once $_SERVER['DOCUMENT_ROOT'].'/viraindo/connection/databaseconnect.php';
+print_r("test");die();
 require_once $_SERVER['DOCUMENT_ROOT'].'/viraindo/auth/AuthMiddleware.php';
 require_once $_SERVER['DOCUMENT_ROOT'].'/viraindo/function.php';
 require '../../vendor/autoload.php';
