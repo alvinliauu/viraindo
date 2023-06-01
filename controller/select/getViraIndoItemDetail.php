4570eb7aecb7ce3833ae0c3fe3217897bb0d21df
alvin <alvinliauu@gmail.com>
2023-06-01 22:39:02 +0700

    test

--- controller/select/getViraIndoItemDetail.php
+++ controller/select/getViraIndoItemDetail.php
@@ -43,6 +43,9 @@
 
             $category_name = $row['category_name'];
 
+            print_r($row);
+            die();
+
             $QueryOfItemLainnya = "SELECT TVI.item_name, TVI.item_picture, TVI.item_new_price 
             FROM tbl_viraindo_category TVC JOIN tbl_viraindo_sub_category TVSC ON TVC.category_id = TVSC.category_id 
             JOIN tbl_viraindo_item TVI ON TVSC.sub_category_id = TVI.sub_category_id WHERE TVC.category_name = '$category_name'
