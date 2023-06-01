f3a2e34a468bb9643a56954ea7ad3e4ccd3fa46c
alvin <alvinliauu@gmail.com>
2023-06-01 22:06:21 +0700

    test

--- controller/select/getViraIndoItemDetail.php
+++ controller/select/getViraIndoItemDetail.php
@@ -24,10 +24,11 @@
 
             $sqlQuery = "SELECT TVC.category_name, TVSC.sub_category_name, TVI.item_name, TVI.item_picture, TVI.item_new_price 
             FROM tbl_viraindo_category TVC JOIN tbl_viraindo_sub_category TVSC ON TVC.category_id = TVSC.category_id 
-            JOIN tbl_viraindo_item TVI ON TVSC.sub_category_id = TVI.sub_category_id WHERE TVI.item_id = 2;";
+            JOIN tbl_viraindo_item TVI ON TVSC.sub_category_id = TVI.sub_category_id WHERE TVI.item_id = '$this->id';";
                             
             $stmt = $this->conn->prepare($sqlQuery);
-            $test = $stmt->execute();
+            $stmt->execute();
+            $test = $stmt->get_result();
 
             print_r($test);die();
             return $stmt;                       
