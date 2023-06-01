83c7bd5ffebde91e7bd990c78e0718e71417e4d3
alvin <alvinliauu@gmail.com>
2023-06-01 22:16:43 +0700

    test

--- controller/select/getViraIndoItemDetail.php
+++ controller/select/getViraIndoItemDetail.php
@@ -39,15 +39,9 @@
                             
             $stmt = $this->conn->prepare($sqlQuery);
             $stmt->execute();
-            $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
             print_r("test");die();
 
-            $QueryOfItemLainnya = "SELECT TVC.category_name, TVSC.sub_category_name, TVI.item_name, TVI.item_picture, TVI.item_new_price 
-            FROM tbl_viraindo_category TVC JOIN tbl_viraindo_sub_category TVSC ON TVC.category_id = TVSC.category_id 
-            JOIN tbl_viraindo_item TVI ON TVSC.sub_category_id = TVI.sub_category_id WHERE item_id = '$this->id'
-            ORDER BY RAND() LIMIT 10;";
-            print_r("test");die();
 
 
             $stmt = $this->conn->prepare($sqlQuery);
