4c54612dd71c6e8b7e2a25e1a83ca9e3e1290a39
alvin <alvinliauu@gmail.com>
2023-06-03 17:06:52 +0700

    test filter search page

--- controller/select/getViraIndoSearchPage.php
+++ controller/select/getViraIndoSearchPage.php
@@ -1,61 +1,153 @@
 <?php
+
+    require_once $_SERVER['DOCUMENT_ROOT'].'/viraindo/repository/filter.php';
+
     class getViraIndoSearchPage{
         // Connection
         private $conn;
         public $id;
         public $name;
+        public $filter;
+        public $category;
+        public $price;
 
         // Db connection
         public function __construct(
-            $db
+            $db, $name, $price
             ){
                 $this->conn = $db;
+                $this->name = $name;
+                $this->price = $price;
             }
 
             // GET ALL
-            public function getViraIndoSearchPage(){
+            public function getViraIndoItemFilter(){
 
-            $jsonInput = json_decode(file_get_contents("php://input"), true);            
-            $this->name = $jsonInput['name'];
-
-            $arr = explode(" ", $this->name);
-
-            if($jsonInput == null){
-                echo "item not found";
-            }
-            else{
-                if($arr[0] == true){
+                if($this->name[0] == true){
+                    
                     $arrTotal = "";
-                    foreach($arr as $index => $count){
+                    foreach($this->name as $index => $count){
                         if($index == 0){
-                            $arrTotal .= "SELECT item_id, item_name, item_new_price, item_picture FROM tbl_viraindo_item WHERE
-                            item_name like '%$count%' ";
+                            $arrTotal .= "SELECT TVI.item_id, TVI.item_name, TVI.item_picture, TVI.item_new_price,
+                            FROM tbl_viraindo_item TVI WHERE TVI.item_name LIKE '%$count%'
+                            ";
                             continue;
                         }
-                        $arrLoop = "AND item_name like '%$count%' ";                             
+                        $arrLoop = "AND TVI.item_name LIKE '%$count%'";                             
                     
                         $arrTotal .= $arrLoop;
-                    }           
-    
-                    $sqlQuery = "$arrTotal;";
-    
+                    }     
+                    $sqlQuery = "$arrTotal ORDER BY TVI.item_new_price $this->price;";
+
                     $stmt = $this->conn->prepare($sqlQuery);
-                    $stmt->execute();
-                    return $stmt;
-                }
-                else{
-                    $sqlQuery = "SELECT item_id, item_name, item_new_price, item_picture FROM tbl_viraindo_item WHERE 
-                    item_name like '%$this->name%';";
-                    $stmt = $this->conn->prepare($sqlQuery);
-                    
                     $stmt->execute();
                     return $stmt;
                 }                
+                else{
+                    $sqlQuery = "SELECT TVI.item_id, TVI.item_name, TVI.item_picture, TVI.item_new_price,
+                    FROM tbl_viraindo_item TVI ORDER BY TVI.item_new_price $this->price;";
+                    $stmt = $this->conn->prepare($sqlQuery);
+                    
+                    $stmt->execute();
+                    return $stmt;
+                }    
+
+                // $jsonInput = json_decode(file_get_contents("php://input"), true);
+                // $this->id = $jsonInput['id'];
+                // $this->filter = $jsonInput['filter'];
+                // $this->price = $jsonInput['price'];
+
+                // if(isset($this->filter)){
+                //     foreach ($this->filter as $filt){
+                //         $name = $filt["name"];
+                        
+                //         $arr[] = $name;
+                //     }
+                // }
+
+                // if($this->price == ""){
+                //     $this->price = "asc";
+                // }
+
+                // if($jsonInput == null){
+                //     echo "item not found";
+                // }
+                // else{
+                    // if($arr[0] == true){
+                    //     $arrTotal = "";
+                    //     foreach($arr as $index => $count){
+                    //         if($index == 0){
+                    //             $arrTotal .= "SELECT TVC.category_name, TVSC.sub_category_id, TVSC.sub_category_name, GROUP_CONCAT(TVI.item_id ORDER BY TVI.item_new_price $this->price SEPARATOR '$^$') AS item_id, GROUP_CONCAT(TVI.item_name ORDER BY TVI.item_new_price $this->price SEPARATOR '$^$') AS item_name,
+                    //             GROUP_CONCAT(TVI.item_picture ORDER BY TVI.item_new_price $this->price SEPARATOR '$^$') AS item_picture, GROUP_CONCAT(TVI.item_new_price ORDER BY TVI.item_new_price $this->price SEPARATOR '$^$') AS item_price
+                    //             FROM tbl_viraindo_item TVI JOIN tbl_viraindo_sub_category TVSC ON TVI.sub_category_id = TVSC.sub_category_id
+                    //             JOIN tbl_viraindo_category TVC ON TVC.category_id = TVSC.category_id
+                    //             WHERE TVI.item_name LIKE '%$count%'
+                    //             ";
+                    //             continue;
+                    //         }
+                    //         $arrLoop = "AND TVI.item_name LIKE '%$count%'";                             
+                        
+                    //         $arrTotal .= $arrLoop;
+                    //     }           
+        
+                    //     $sqlQuery = "$arrTotal AND TVSC.sub_category_id = $this->id GROUP BY TVSC.sub_category_id;";
+        
+                    //     $stmt = $this->conn->prepare($sqlQuery);
+                    //     $stmt->execute();
+                    //     return $stmt;
+                    // }                
+                    // else{
+                    //     $sqlQuery = "SELECT TVC.category_name, TVSC.sub_category_id, TVSC.sub_category_name, GROUP_CONCAT(TVI.item_id ORDER BY TVI.item_new_price $this->price SEPARATOR '$^$') AS item_id, GROUP_CONCAT(TVI.item_name ORDER BY TVI.item_new_price $this->price SEPARATOR '$^$') AS item_name,
+                    //     GROUP_CONCAT(TVI.item_picture ORDER BY TVI.item_new_price $this->price SEPARATOR '$^$') AS item_picture, GROUP_CONCAT(TVI.item_new_price ORDER BY TVI.item_new_price $this->price SEPARATOR '$^$') AS item_price
+                    //     FROM tbl_viraindo_item TVI JOIN tbl_viraindo_sub_category TVSC ON TVI.sub_category_id = TVSC.sub_category_id
+                    //     JOIN tbl_viraindo_category TVC ON TVC.category_id = TVSC.category_id
+                    //     WHERE TVSC.sub_category_id = $this->id GROUP BY TVSC.sub_category_id;";
+                    //     $stmt = $this->conn->prepare($sqlQuery);
+                        
+                    //     $stmt->execute();
+                    //     return $stmt;
+                    // }                
+                // }
+
             }
 
+            public function getViraIndoItemFilterItemNull(){
+            
+                $jsonInput = json_decode(file_get_contents("php://input"), true);
+                $this->name = $jsonInput['name'];
+                $this->filter = $jsonInput['filter'];
+                $this->price = $jsonInput['price'];
 
+                if(isset($this->filter)){
+                    foreach ($this->filter as $filt){
+                        $name = $filt["name"];
+                        
+                        $arr[] = $name;
+                    }
+                }
 
-        }
+                if($this->price == ""){
+                    $this->price = "asc";
+                }
+
+                if($jsonInput == null){
+                    echo "item not found";
+                }
+                else{
+               
+        
+                    $sqlQuery = "SELECT TVC.category_name, TVSC.sub_category_id, TVSC.sub_category_name
+                    FROM tbl_viraindo_item TVI JOIN tbl_viraindo_sub_category TVSC ON TVI.sub_category_id = TVSC.sub_category_id
+                    JOIN tbl_viraindo_category TVC ON TVC.category_id = TVSC.category_id
+                    WHERE TVSC.sub_category_id = $this->id;";
+        
+                    $stmt = $this->conn->prepare($sqlQuery);
+                    $stmt->execute();
+                    return $stmt;
+                             
+                }
+
+            }
 
     }
 ?>
--- main/select/getViraIndoSearchPage/index.php
+++ main/select/getViraIndoSearchPage/index.php
@@ -4,43 +4,87 @@
     header("Access-Control-Max-Age: 3600");
     header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
+    require_once $_SERVER['DOCUMENT_ROOT'].'/viraindo/repository/filter.php';
+
     include_once '../../../connection/databaseconnect.php';
     include_once '../../../controller/select/getViraIndoSearchPage.php';
 
     $database = new Database();
     $db = $database->getConnection();
-    $item = new getViraIndoSearchPage($db);
 
-    $stmt = $item->getViraIndoSearchPage();
+    $jsonInput = json_decode(file_get_contents("php://input"), true);
+    $name = $jsonInput['name'];
+    $price = $jsonInput['price'];
+
+    if(isset($name)){
+        if(empty($name)){
+            $arr[] = "";
+        } else {
+            foreach ($name as $filt){
+                $name = $filt["name"];
+                
+                $arr[] = $name;
+            }
+        }
+    }
+
+    if($price == ""){
+        $price = "asc";
+    }
+
+    $item = new getViraIndoSearchPage($db, $arr, $price);
+    
+    $stmt = $item->getViraIndoItemFilter();
     $itemCount = $stmt->rowCount();
+
     $productArr = array();
 
     if($itemCount > 0){
-        
-        // $productArr["list"] = array();
+    
 
         while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-
             // create array
             extract($row);
-                $e = array(
-                    "id" => $item_id,
-                    "name" => $item_name,
-                    "price" => $item_new_price,
-                    "image" => array(
-                        "url" => $item_picture,
-                        "alt" => "viraindo"
-                    )
-                );
-                array_push($productArr, $e);
+
+            $results = [];
+
+            $theArray = array("id" => $item_id, "name" => $item_name, "price" => $item_price, "image" => array("url" => $item_picture, "alt" => "viraindo"));
+
+            array_push($results, $theArray);
+
+            $e = array(
+                "filter" => filter($category_name, $arr),
+                "item" => $results
+            );
+            array_push($productArr, $e);
           
             http_response_code(200);
         }
+
         echo json_encode($productArr);
     }
       
     else{
-        http_response_code(404);
+
+        $stmtElse = $item->getViraIndoItemFilterItemNull();
+
+        while($row = $stmtElse->fetch(PDO::FETCH_ASSOC)){
+            // create array
+            extract($row);
+
+            $results = [];
+
+            $e = array(
+                "id" => $sub_category_id,
+                "name" => $sub_category_name,
+                "filter" => filter($category_name, $arr),
+                "item" => $results
+            );
+            
+            http_response_code(200);
+        }
+        array_push($productArr, $e);
+
         echo json_encode($productArr);
     }
 ?>
