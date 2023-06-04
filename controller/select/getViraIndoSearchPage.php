60e42f2dfea2fdff19b72b77b8fdeedf672d7e28
alvin <alvinliauu@gmail.com>
2023-06-04 12:33:51 +0700

    test

--- controller/select/getViraIndoSearchPage.php
+++ controller/select/getViraIndoSearchPage.php
@@ -62,9 +62,6 @@
                     else{
                         $sqlQuery = "$arrTotal ORDER BY TVI.item_new_price $this->price;";
                     }                    
-                    
-                    print_r($sqlQuery);
-                    die();
 
                     $stmt = $this->conn->prepare($sqlQuery);
                     $stmt->execute();
