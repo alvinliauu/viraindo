fa27f1a76304d8500e77431c92041bbf4a115290
alvin <alvinliauu@gmail.com>
2023-07-09 20:30:50 +0700

    subcategory dropdown

--- controller/select/getViraIndoSubCategoryList.php
+++ controller/select/getViraIndoSubCategoryList.php
@@ -15,9 +15,12 @@
 
             // GET ALL
             public function getViraIndoSubCategoryList(){
+
+                $jsonInput = json_decode(file_get_contents("php://input"), true);
+                $this->id = $jsonInput["id"];
      
-                $sqlQuery = "SELECT sub_category_id, sub_category_name
-                FROM tbl_viraindo_sub_category;";
+                $sqlQuery = "SELECT category_id, sub_category_id, sub_category_name
+                FROM tbl_viraindo_sub_category WHERE category_id = '$this->id';";
         
                 $stmt = $this->conn->prepare($sqlQuery);
                 $stmt->execute();
