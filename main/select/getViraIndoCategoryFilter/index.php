42c6cfca92f0fa1b779193739f9f7329e4c95b3a
alvin <alvinliauu@gmail.com>
2023-07-25 22:29:25 +0700

    test

--- main/select/getViraIndoCategoryFilter/index.php
+++ main/select/getViraIndoCategoryFilter/index.php
@@ -82,7 +82,7 @@ if ($itemCount > 0) {
 
         foreach ($explodeItemName as $key => $value) {
 
-            if ($row['$category_name'] == 'notebook') {
+            if ($row['category_name'] == 'Notebook') {
 
                 $start_pos = strposa($value, $start, 1);
                 $end_pos = strpos($value, $end, $start_pos);
